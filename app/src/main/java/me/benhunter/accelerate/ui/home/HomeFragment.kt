c3470907d7eaceebc16bf291f1a25589beacda5f
Ben Hunter <code@benhunter.me>
2022-11-20 17:20:21 -0600

    Buttons to confirm logout

--- app/src/main/java/me/benhunter/accelerate/ui/home/HomeFragment.kt
+++ app/src/main/java/me/benhunter/accelerate/ui/home/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.recyclerview.widget.LinearLayoutManager
 import me.benhunter.accelerate.MainViewModel
 import me.benhunter.accelerate.databinding.FragmentHomeBinding
 
