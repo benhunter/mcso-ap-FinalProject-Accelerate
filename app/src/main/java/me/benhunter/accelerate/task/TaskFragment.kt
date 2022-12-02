d34d35c00f81fabd97b025f4ec6516b41d3f8a6f
Ben Hunter <code@benhunter.me>
2022-12-01 23:58:04 -0600

    Add Task due date and time layouts with Pickers

--- app/src/main/java/me/benhunter/accelerate/task/TaskFragment.kt
+++ app/src/main/java/me/benhunter/accelerate/task/TaskFragment.kt
@@ -1,18 +1,23 @@
 package me.benhunter.accelerate.task
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import me.benhunter.accelerate.databinding.FragmentTaskBinding
 import me.benhunter.accelerate.board.BoardViewModel
 import me.benhunter.accelerate.category.CategoryViewModel
+import me.benhunter.accelerate.databinding.FragmentTaskBinding
+import java.util.*
+
 
 class TaskFragment : Fragment() {
     private val TAG = javaClass.simpleName
@@ -52,9 +57,11 @@ class TaskFragment : Fragment() {
 
         binding.taskSaveButton.setOnClickListener {
             val categorySelectedPosition = binding.taskCategorySpinner.selectedItemPosition
-            val categoryIdNullable = boardViewModel.observeCategories().value?.get(categorySelectedPosition)?.firestoreId
+            val categoryIdNullable =
+                boardViewModel.observeCategories().value?.get(categorySelectedPosition)?.firestoreId
             categoryIdNullable?.let { categoryId ->
-                taskViewModel.saveNameAndCategory(binding.taskNameEdittext.text.toString(),
+                taskViewModel.saveNameAndCategory(
+                    binding.taskNameEdittext.text.toString(),
                     categoryId
                 )
             }
@@ -87,5 +94,56 @@ class TaskFragment : Fragment() {
             }
         }
 
+        binding.taskSetDateButton.setOnClickListener {
+            showDatePickerDialog()
+        }
+
+        binding.taskSetTimeButton.setOnClickListener{
+            showTimePickerDialog()
+        }
+
+    }
+
+    // Credit https://www.digitalocean.com/community/tutorials/android-date-time-picker-dialog
+    @SuppressLint("SetTextI18n")
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val currentYear = calendar.get(Calendar.YEAR)
+        val currentMonth = calendar.get(Calendar.MONTH)
+        val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this.requireContext(),
+            { _, year, monthOfYear, dayOfMonth ->
+                val day = dayOfMonth.toString().padStart(2, '0')
+                val month = (monthOfYear + 1).toString().padStart(2, '0')
+                binding.taskDueDateEdittext.setText("$year-$month-$day")
+            },
+            currentYear,
+            currentMonth,
+            currentDay
+        )
+        datePickerDialog.show()
+    }
+
+    // Credit https://www.digitalocean.com/community/tutorials/android-date-time-picker-dialog
+    @SuppressLint("SetTextI18n")
+    private fun showTimePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val currentHour = calendar.get(Calendar.HOUR_OF_DAY)
+        val currentMinute = calendar.get(Calendar.MINUTE)
+
+        val timePickerDialog = TimePickerDialog(
+            this.requireContext(),
+            { _, hour, minute ->
+                val hourPadded = hour.toString().padStart(2, '0')
+                val minutePadded = minute.toString().padStart(2, '0')
+                binding.taskDueTimeEdittext.setText("$hourPadded:$minutePadded")
+            },
+            currentHour,
+            currentMinute,
+            true
+        )
+        timePickerDialog.show()
     }
 }
